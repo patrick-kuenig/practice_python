58d7600aae581cd29a832afaea24c7a634139509
patrick-kuenig <kuenig.patrick@gmail.com>
2024-04-29 11:33:48 +0300

    Exercise 1 in progress

---
+++ Exercise_1.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name.\n")
+age = input("How old are you?\n")
+current_year = 2024
+birth_year = current_year - int(age)
+
+print(f"Hi {name}. I see you'll turn 100 in the year {birth_year+100}, good for you.")
